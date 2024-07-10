c8a4075f4325edb4e91dc2eaa68f8c1689acd97c
Sterling Welker <sterlingjdub@gmail.com>
2024-07-10 16:20:33 -0600

    Updated controllers for error handling

--- controllers/classesController.js
+++ controllers/classesController.js
@@ -2,76 +2,85 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getClasses = async (req, res) => {
-    const result = await mongodb.getDb().db().collection('Classes').find();
-    result.toArray().then((err, lists) => {
-        if (err) {
-            res.status(400).json({message: err});
-        }
+    try {
+        const result = await mongodb.getDb().db().collection('Classes').find().toArray();
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists);
-    });
+        res.status(200).json(result);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 };
 
 const getClassById = async (req, res) => {
     try {
         const classId = new ObjectId(req.params.id);
-        const result = await mongodb.getDb().db().collection('Classes').find({_id:classId});
+        const result = await mongodb.getDb().db().collection('Classes').findOne({ _id: classId });
         if (!result) {
-            return res.status(404).json({message: "Class not found"});
+            return res.status(404).json({ message: "Class not found" });
         }
-        result.toArray().then((lists) => {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(lists[0]);
-        });
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(result);
     } catch (error) {
         console.error("Error getting class by ID:", error);
-        res.status(500).json({message: "Internal server error"});
+        res.status(500).json({ message: "Internal server error" });
     }
 };
 
 const createClass = async (req, res) => {
-    const fitnessClass = {
-        className: req.body.className,
-        instructor: req.body.instructor,
-        schedule: req.body.schedule,
-        duration: req.body.duration,
-        capacity: req.body.capacity,
-        location: req.body.location
-    };
-    const response = await mongodb.getDb().db().collection('Classes').insertOne(fitnessClass);
-    if (response.acknowledged) {
-      res.status(201).json(response);
-    } else {
-      res.status(500).json(response.error || 'Some error occurred while creating the class.');
+    try {
+        const fitnessClass = {
+            className: req.body.className,
+            instructor: req.body.instructor,
+            schedule: req.body.schedule,
+            duration: req.body.duration,
+            capacity: req.body.capacity,
+            location: req.body.location
+        };
+        const response = await mongodb.getDb().db().collection('Classes').insertOne(fitnessClass);
+        if (response.acknowledged) {
+            res.status(201).json(response);
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while creating the class.');
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 };
 
-const udpateClass = async (req, res) => {
-    const classId = new ObjectId(req.params.id);
-    const fitnessClass = {
-        className: req.body.className,
-        instructor: req.body.instructor,
-        schedule: req.body.schedule,
-        duration: req.body.duration,
-        capacity: req.body.capacity,
-        location: req.body.location
-    };
-    const response = await mongodb.getDb().db().collection('Classes').replaceOne({_id:classId}, fitnessClass);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occured while updating the class.');
-    };
+const updateClass = async (req, res) => {
+    try {
+        const classId = new ObjectId(req.params.id);
+        const fitnessClass = {
+            className: req.body.className,
+            instructor: req.body.instructor,
+            schedule: req.body.schedule,
+            duration: req.body.duration,
+            capacity: req.body.capacity,
+            location: req.body.location
+        };
+        const response = await mongodb.getDb().db().collection('Classes').replaceOne({ _id: classId }, fitnessClass);
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while updating the class.');
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 const deleteClass = async (req, res) => {
-    const classId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db().collection('Classes').deleteOne({_id:classId}, true);
-    if (result.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(result.error || 'Some error occured while deleting the class.');
-    };
+    try {
+        const classId = new ObjectId(req.params.id);
+        const result = await mongodb.getDb().db().collection('Classes').deleteOne({ _id: classId });
+        if (result.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(result.error || 'Some error occurred while deleting the class.');
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 module.exports = {
--- controllers/membersController.js
+++ controllers/membersController.js
@@ -2,78 +2,87 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getMembers = async (req, res) => {
-    const result = await mongodb.getDb().db().collection('Members').find();
-    result.toArray().then((err, lists) => {
-        if (err) {
-            res.status(400).json({message: err});
-        }
+    try {
+        const result = await mongodb.getDb().db().collection('Members').find().toArray();
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists);
-    });
+        res.status(200).json(result);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 };
 
 const getMemberById = async (req, res) => {
     try {
         const memberId = new ObjectId(req.params.id);
-        const result = await mongodb.getDb().db().collection('Members').find({_id:memberId});
+        const result = await mongodb.getDb().db().collection('Members').findOne({ _id: memberId });
         if (!result) {
-            return res.status(404).json({message: "Member not found"});
+            return res.status(404).json({ message: "Member not found" });
         }
-        result.toArray().then((lists) => {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(lists[0]);
-        });
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(result);
     } catch (error) {
         console.error("Error getting member by ID:", error);
-        res.status(500).json({message: "Internal server error"});
+        res.status(500).json({ message: "Internal server error" });
     }
 };
 
 const createMember = async (req, res) => {
-    const member = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        phone: req.body.phone,
-        dateOfBirth: req.body.dateOfBirth,
-        membershipType: req.body.membershipType,
-        startDate: req.body.startDate
-    };
-    const response = await mongodb.getDb().db().collection('Members').insertOne(member);
-    if (response.acknowledged) {
-      res.status(201).json(response);
-    } else {
-      res.status(500).json(response.error || 'Some error occurred while creating the member.');
+    try {
+        const member = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            phone: req.body.phone,
+            dateOfBirth: req.body.dateOfBirth,
+            membershipType: req.body.membershipType,
+            startDate: req.body.startDate
+        };
+        const response = await mongodb.getDb().db().collection('Members').insertOne(member);
+        if (response.acknowledged) {
+            res.status(201).json(response);
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while creating the member.');
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 };
 
 const updateMember = async (req, res) => {
-    const memberId = new ObjectId(req.params.id);
-    const member = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        phone: req.body.phone,
-        dateOfBirth: req.body.dateOfBirth,
-        membershipType: req.body.membershipType,
-        endDate: req.body.endDate
-    };
-    const response = await mongodb.getDb().db().collection('Members').replaceOne({_id:memberId}, member);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occured while updating the member.');
-    };
+    try {
+        const memberId = new ObjectId(req.params.id);
+        const member = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            phone: req.body.phone,
+            dateOfBirth: req.body.dateOfBirth,
+            membershipType: req.body.membershipType,
+            endDate: req.body.endDate
+        };
+        const response = await mongodb.getDb().db().collection('Members').replaceOne({ _id: memberId }, member);
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while updating the member.');
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 const deleteMember = async (req, res) => {
-    const memberId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db().collection('Members').deleteOne({_id:memberId}, true);
-    if (result.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(result.error || 'Some error occured while deleting the member.');
-    };
+    try {
+        const memberId = new ObjectId(req.params.id);
+        const result = await mongodb.getDb().db().collection('Members').deleteOne({ _id: memberId });
+        if (result.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(result.error || 'Some error occurred while deleting the member.');
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 module.exports = {
